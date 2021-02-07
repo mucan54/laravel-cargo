c34601c2627781907f6d60fac29de96897bfd433
Can Os <canoz@Can-MacBook-Air.home>
2021-02-07 22:48:28 +0300

    update

--- resources/views/brand/footer.blade.php
+++ resources/views/brand/footer.blade.php
@@ -1,3 +1,3 @@
 <p class="small m-n">
-    © Copyright {{date('Y')}} <a href="//example.com" target="_blank">"Analytics Nest"</a>
+    © Copyright {{date('Y')}} <a href="http://tonkul.com.tr" target="_blank">Tokul</a>
 </p>
